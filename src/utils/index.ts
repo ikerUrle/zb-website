156e6a430efbc1d37c655e6c686b25621feb2235
ikerUrle <iker@urle.eus>
2024-04-25 10:27:28 +0000

    datak eguneratu

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,59 @@
 export type Date = {
 	city: string,
 	date: string | null,
-	extra: string,
+	extra?: string,
+	hide?: boolean
 }
 export const dates: Date[] = [
 	{
-		city: "Mungia",
-		date: "2024-06-15",
-		extra: ""
+		city: "Zamudio",
+		date: "2024-05-25",
 	},
 	{
-		city: "Gernika",
-		date: "2024-01-15",
-		extra: ""
+		city: "Erandio",
+		date: "2024-05-31",
+	},
+	{
+		city: "Igorre",
+		date: "2024-06-08",
+	},
+	{
+		city: "La Quadra",
+		date: "2024-06-29",
+		hide: true,
+	},
+	{
+		city: "Foru",
+		date: "2024-07-26",
+	},
+	{
+		city: "Lurgorri",
+		date: "2024-08-10",
+		hide: true,
+	},
+	{
+		city: "Luhuso",
+		date: "2024-08-16",
+	},
+	{
+		city: "Bilbo",
+		date: "2024-08-21",
+	},
+	{
+		city: "Laudio",
+		date: "2024-08-23",
+	},
+	{
+		city: "Larrabetzu",
+		date: "2024-08-30",
+	},
+	{
+		city: "Orozko",
+		date: "2024-08-31",
+	},
+	{
+		city: "Otxagabia",
+		date: "2024-09-06",
 	},
 	{
 		city: "",
