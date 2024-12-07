7a1d01673fbb16eee19e17261cae7e0b9479311f
ikerUrle <iker@urle.eus>
2024-12-07 16:07:42 +0100

    feat: 2025 prestatzen...

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
 		<ul class="flex gap-4 flex-wrap pb-2">
 			<ContactButton icon="i-carbon-phone" image="phone" text="659 975 820" link="tel:+34659975820"/>
 			<ContactButton icon="i-carbon-logo-instagram" image="instagram" text="zirkinikbez" link="https://www.instagram.com/zirkinikbez/" />
-			<ContactButton icon="i-carbon-email" image="email" text="info@erromeriak.eus" link="mailto:info@erromeriak.eus"/>
+			<ContactButton icon="i-carbon-email" image="email" text="info@baga-biga.eus" link="mailto:info@baga-biga.eus"/>
 		</ul>
 </template>
 
---
+++ src/components/Modal15Urte.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="modal absolute inset-0 z-2000 flex flex-col items-center justify-center"
+  >
+    <div class="text-red-400 uppercase font-black text-6xl sm:text-8xl">15 urte</div>
+    <div class="text-light-900">2025 - #HankazGora</div>
+    <div class="text-dark-100">Prestatzen...</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.modal {
+  background-color: rgba(0, 0, 0, 0.95);
+}
+</style>
+
