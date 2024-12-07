977c3799e36dd01176268fe61134b0472b9f07c1
ikerUrle <iker@urle.eus>
2024-12-07 18:02:35 +0100

    feat: kolorea aldatzen

--- src/components/Modal15Urte.vue
+++ src/components/Modal15Urte.vue
@@ -3,11 +3,11 @@
     class="modal absolute inset-0 z-2000 flex flex-col items-center justify-center"
   >
     <div class="text-red-400 uppercase font-black text-6xl sm:text-8xl">15 urte</div>
-    <div class="text-light-900">2025 - #HankazGora</div>
     <div class="text-2xl">
       <Contact />
     </div>
-    <div class="text-#888">Prestatzen...</div>
+    <div class="text-light-900">2025 - #HankazGora</div>
+    <div class="text-#666">Prestatzen...</div>
   </div>
 </template>
 
@@ -18,7 +18,7 @@ import Contact from './Contact.vue';
 
 <style scoped>
 .modal {
-  background-color: rgba(0, 0, 0, 0.95);
+  background-color: rgba(0, 0, 0, 0.90);
 }
 </style>
 
