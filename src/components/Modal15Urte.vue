cde0a3bcb4ae3d7247cc23633132af01f4c7424c
ikerUrle <iker@urle.eus>
2025-02-16 16:57:33 +0100

    feat: hankaz gora

--- src/components/Modal15Urte.vue
+++ src/components/Modal15Urte.vue
@@ -4,10 +4,10 @@
   >
     <div class="text-red-400 uppercase font-black text-4xl sm:text-8xl py-2">Bira hasiera!</div>
     <div class="text-xl sm:text-4xl text-#aaa">Otsailak 28 - Astra (Gernika)</div>
-    <div class="text-2xl sm:text-4xl">
+    <div class="text-#666 sm:text-2xl">15 urte - #HankazGora</div>
+    <div class="text-2xl sm:text-4xl py-2">
       <Contact />
     </div>
-    <div class="text-#666 sm:text-2xl">Prestatzen...</div>
   </div>
 </template>
 
